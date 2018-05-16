f8f68d04ffe6ac0622d20f1f2b53c2be74aeedfb
yuanxc13 <yuanxc13@163.com>
2018-05-16 16:05:32 +0800

    modify filling na method

--- src/tools/data.py
+++ src/tools/data.py
@@ -11,10 +11,31 @@ def object2timestamp(timestampStr):
 
 def readBeijingAq(filename):
   df = pd.read_csv(filename)
-  future_num = 4
+  past_aq_pick_num = 4
   # get numpy array
   values = df.values
   total = len(values)
+
+  # fill na
+  max_gap = 3
+  for j in range(6):
+    begin = -1
+    for index in range(total):
+      if not np.isnan(values[index][j + 2]):
+        begin = index
+        break
+    if begin == -1:
+      continue
+    while begin < total:
+      end = begin + 1
+      while end < total and np.isnan(values[end][j + 2]):
+        end += 1
+      if end - begin <= max_gap + 1 and end < total:
+        for index in range(begin + 1, end):
+          values[index][j + 2] = (values[end][j + 2] - values[begin][j + 2]) * (index - begin) / (end - begin) + values[begin][j + 2]
+      begin = end
+
+  # filter empty data
   proper_data = {}
   for i in range(1, total):
     proper = True
@@ -26,8 +47,9 @@ def readBeijingAq(filename):
       if not values[i][0] in proper_data:
         proper_data[values[i][0]] = []
       proper_data[values[i][0]].append((object2timestamp(values[i][1]), values[i][2:]))
+  
   dataset = {}
-  for station in proper_data.keys():
+  for station in proper_data:
     aq_data = proper_data[station]
     dataset[station] = [[], []]
     begin_index = 0
@@ -36,15 +58,11 @@ def readBeijingAq(filename):
       end_index = begin_index + 1
       while end_index < len(aq_data) and (aq_data[end_index][0] - aq_data[end_index - 1][0] == 1):
         end_index += 1
-      for begin in range(begin_index, end_index - future_num - 1):
+      for begin in range(begin_index, end_index - past_aq_pick_num - 1):
         data = []
-        for index in range(begin, begin + future_num):
+        for index in range(begin, begin + past_aq_pick_num):
           data += list(aq_data[index][1])
         dataset[station][0].append(data)
-        dataset[station][1].append(aq_data[begin + future_num][1])
+        dataset[station][1].append(aq_data[begin + past_aq_pick_num][1])
       begin_index = end_index
   return dataset
-
-if __name__ == '__main__':
-  dataset = readBeijingAq('../../data/beijing_17_18_aq.csv')
-
--- src/tools/train.py
+++ src/tools/train.py
@@ -7,30 +7,36 @@ def smape(actual, predicted):
     a = np.abs(np.array(actual) - np.array(predicted))
     b = np.array(actual) + np.array(predicted)
     return 2 * np.mean(np.divide(a, b, out=np.zeros_like(a), where=b!=0, casting='unsafe'))
-
       
 def train_interface(dataset, method):
   print('train with model ' + method)
-  for station in dataset:
-    data = dataset[station]
-    train_num = math.floor(len(data) * 0.8)
-    train_features = data[0][:train_num]
-    train_tags = data[1][:train_num]
-    test_features = data[0][train_num:]
-    test_tags = data[1][train_num:]
-    model = None
-    if method == 'linear regression':
-      model = linear_model.LinearRegression()
-    if not model:
-      print("incorrect model")
-      return
-    model.fit(train_features, train_tags)
-    predicted = model.predict(test_features)
-    for i in range(len(predicted)):
-      for j in range(len(predicted[i])):
-        predicted[i][j] = max(predicted[i][j], 0.0)
-    score = smape(test_tags, predicted)
-    print('station : ' + station + ' test score: ', score)
+  data = process_dataset(dataset)
+  print('datasize: ' + str(len(data[0])))
+  train_num = math.floor(len(data[0]) * 0.8)
+  train_features = data[0][:train_num]
+  train_tags = data[1][:train_num]
+  test_features = data[0][train_num:]
+  test_tags = data[1][train_num:]
+  model = None
+  if method == 'linear regression':
+    model = linear_model.LinearRegression()
+  if not model:
+    print("incorrect model")
+    return
+  model.fit(train_features, train_tags)
+  predicted = model.predict(test_features)
+  for i in range(len(predicted)):
+    for j in range(len(predicted[i])):
+      predicted[i][j] = max(predicted[i][j], 0.0)
+  score = smape(test_tags, predicted)
+  print('test score: ', score)
+
+def process_dataset(dataset):
+  ret = [[], []]
+  for key in dataset:
+    ret[0] += dataset[key][0]
+    ret[1] += dataset[key][1]
+  return ret
 
 if __name__ == '__main__':
   dataset = data.readBeijingAq('../../data/beijing_17_18_aq.csv')
